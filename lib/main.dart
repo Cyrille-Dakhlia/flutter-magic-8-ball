50970a4cf19cf7a037edf3fe2b4a4fc4c3829257
Cyrille Dakhlia <dakhliacyrille@gmail.com>
2022-08-05 21:34:45 +0200

    Add base structure of the app

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,39 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      appBar: AppBar(
+        backgroundColor: Colors.indigo,
+        title: Text('Ask Me Anything'),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: TextButton(
+          onPressed: () => print('pressed'),
+          child: Image.asset('images/ball1.png'),
+        ),
+      ),
+    );
+  }
+}

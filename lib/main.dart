301a8ff9f2077254e5019a6899df4cee1edb7961
Cyrille Dakhlia <dakhliacyrille@gmail.com>
2022-08-05 21:43:24 +0200

    Change font using google_fonts package

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(
       MaterialApp(
@@ -15,7 +16,12 @@ class BallPage extends StatelessWidget {
       backgroundColor: Colors.lightBlueAccent,
       appBar: AppBar(
         backgroundColor: Colors.indigo,
-        title: Text('Ask Me Anything'),
+        title: Text(
+          'Ask Me Anything',
+          style: GoogleFonts.sacramento(
+            fontSize: 38.0,
+          ),
+        ),
       ),
       body: Ball(),
     );
